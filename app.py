c415f809b353ed1f3c7354b5ea0862720fe6bcc3
Sam Baker <rb.sam@icloud.com>
2021-03-11 17:49:36 +0000

    added comments to browse function

--- app.py
+++ app.py
@@ -24,35 +24,34 @@ def index():
 
 @app.route("/browse", methods=["GET", "POST"])
 def browse():
-    beans = mongo.db.beans.find()
-    roast_types = mongo.db.beans.distinct('roast')
-    origin_types = mongo.db.beans.distinct('origin')
+    beans = mongo.db.beans.find() # DEFAULT VIEW SHOWS ALL RESULTS
+    roast_types = mongo.db.beans.distinct('roast') # GETS ALL UNIQUE VALUES WITH KEY OF 'ROAST'
+    origin_types = mongo.db.beans.distinct('origin') # GETS ALL UNIQUE VALUES WITH KEY OF 'ORIGIN'
     if request.method == "POST":
         roastChecked = []
         originChecked = []
-        for item in request.form:
+        for item in request.form: # APPENDS ALL KEY VALUE PAIRS TO THEIR OWN ARRAYS
             checkboxReturn = item.split('=')
             if checkboxReturn[0] == 'roast':
                 roastChecked.append(checkboxReturn[1])
             if checkboxReturn[0] == 'origin':
                 originChecked.append(checkboxReturn[1])
 
-
+        # DYNAMICALLY CREATES A FIND QUERY
         dynamicQuery = {}
         dynamicQuery["$and"]=[]
-
+        # CHECKS IF VALUES EXIST AND ADDS TO DYNAMIC QUERY
         if roastChecked:
             dynamicQuery["$and"].append({ "roast": { "$in": roastChecked}})
         if originChecked:
             dynamicQuery["$and"].append({ "origin": { "$in": originChecked }})
         if 'organicRequired' in request.form:
             dynamicQuery["$and"].append({ "organic": True })
-        
+        # REPLACES VIEW WITH DYNAMIC QUERY SET BY USE FILTER INPUT
         beans = mongo.db.beans.find(dynamicQuery)
 
-
-
     return render_template("browse.html", beans=beans, roast_types=roast_types, origin_types=origin_types)
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
