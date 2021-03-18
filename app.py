ced556ccf6264a82ca1071a9e73e17c19248becb
Sam Baker <rb.sam@icloud.com>
2021-03-18 10:45:44 +0000

    added conditional nav items

--- app.py
+++ app.py
@@ -181,6 +181,12 @@ def profile(username):
     
     return redirect(url_for("login"))
 
+@app.route("/logout")
+def logout():
+    flash(u"You have been logged out", "success")
+    session.pop("user")
+    return redirect(url_for("login"))
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
     port=int(os.environ.get("PORT")),
