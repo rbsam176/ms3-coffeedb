457575a3c722486f74459e09ecbc8c4f9f0ce1a1
Sam Baker <rb.sam@icloud.com>
2021-03-24 16:06:51 +0000

    added 404 error page

--- app.py
+++ app.py
@@ -36,6 +36,14 @@ accountPreferences = {
     "site_discovery": ["search engine", "from a friend", "online advertising", "offline advertising"]
 }
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(403)
+def page_not_found(e):
+    return render_template('403.html'), 403
+
 @app.route("/")
 def index():
     beans = mongo.db.beans.find()
