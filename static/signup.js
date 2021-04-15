1891072dd073e6533ba6b8fdd493b6b34a8a19da
Sam Baker <rb.sam@icloud.com>
2021-04-15 19:12:17 +0100

    added password criteria checker functionality

--- static/signup.js
+++ static/signup.js
@@ -28,3 +28,67 @@ $(".swatch-btn").on('click', function(){
         // need to change this because clickedSwatchName is a background-color and it needs to be a color
     }
 })
+
+// PASSWORD LENGTH CHECKER
+function characterLength(iconId){
+    if ($("#inputPassword").val().length > 7 && $("#inputPassword").val().length < 16){
+        // IF IT ISN'T ALREADY FILLED
+        if (!$(iconId).hasClass('bi-check-circle-fill')){
+            $(iconId).removeClass()
+            $(iconId).addClass('bi bi-check-circle-fill')
+        }
+        // IF INPUT FIELD LENGTH IS NOT WITHIN RANGE
+    } else {
+        // IF IT ISN'T ALREADY WITHOUT FILL
+        if (!$(iconId).hasClass('bi-check-circle')){
+            $(iconId).removeClass()
+            $(iconId).addClass('bi bi-check-circle')
+        }
+    }
+}
+
+// UPPERCASE/NUMBER CHECKER
+function regexCheck(iconId, regex){
+    if ($("#inputPassword").val().match(regex)){
+        // IF IT ISN'T ALREADY FILLED
+        if (!$(iconId).hasClass('bi-check-circle-fill')){
+            $(iconId).removeClass()
+            $(iconId).addClass('bi bi-check-circle-fill')
+        }
+    } else {
+        // IF IT ISN'T ALREADY WITHOUT FILL
+        if (!$(iconId).hasClass('bi-check-circle')){
+            $(iconId).removeClass()
+            $(iconId).addClass('bi bi-check-circle')
+        }
+    }
+}
+
+// BOTH PASSWORDS MATCH CHECKER
+function passwordMatch(firstId, secondId, iconId){
+    // TO PREVENT BOTH EMPTY INPUTS BEING A MATCH
+    if ($(firstId).val().length > 0 && $(secondId).val().length > 0){
+        // IF BOTH INPUTS HAVE THE SAME VALUE
+        if ($(firstId).val() == $(secondId).val()){
+            // IF IT ISN'T ALREADY FILLED
+            if (!$(iconId).hasClass('bi-check-circle-fill')){
+                $(iconId).removeClass()
+                $(iconId).addClass('bi bi-check-circle-fill')
+            }
+        } else {
+            // IF IT ISN'T ALREADY WITHOUT FILL
+            if (!$(iconId).hasClass('bi-check-circle')){
+                $(iconId).removeClass()
+                $(iconId).addClass('bi bi-check-circle')
+            }
+        }
+    }
+}
+
+// RUNS PASSWORD CHECKER FUNCTIONS ON PASSWORD INPUT FIELDS
+$("input:password").on('input', function(){
+    characterLength("#characterLength")
+    regexCheck("#uppercaseCheck", "[A-Z]")
+    regexCheck("#numberCheck", "[0-9]")
+    passwordMatch("#inputPassword", "#inputConfirmPassword", "#matchCheck")
+})
