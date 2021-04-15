5893b0d18488c2879fc51a287797daadfb5c6985
Sam Baker <rb.sam@icloud.com>
2021-04-15 13:24:54 +0100

    updated recently added on add page to adjust to breakpoint

--- static/edit.js
+++ static/edit.js
@@ -1,3 +1,4 @@
+// TRIGGERS THE CONFIRMATION DELETE BUTTON TO APPEAR
 $("#initialDelete").on('click', function() {
     $("#initialDelete").css('display', 'none')
     $("#confirmDelete, #confirmMessage").css('display', 'block')
