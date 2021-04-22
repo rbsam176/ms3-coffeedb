3835c9ada35cb43bde21dd30e11bec8b8b688dc5
Sam Baker <rb.sam@icloud.com>
2021-04-22 17:01:39 +0100

    added sequence to index videos

--- static/index.js
+++ static/index.js
@@ -1,1 +1,17 @@
 $("#main").css('display', 'none')
+
+// CYCLES THROUGH BOTH VIDEOS
+$(document).ready(function(){
+    $('video').on('ended',function(){
+        if ($('.primary-video').css('display') == 'block') {
+            $(this).css('display', 'none')
+            $(this).next().css('display', 'block')
+            $(this).next().trigger('play')
+        }
+        if ($('.secondary-video').css('display') == 'block') {
+            $(this).css('display', 'none')
+            $(this).prev().css('display', 'block')
+            $(this).prev().trigger('play')
+        }
+    });
+  });
