9e5a1cebe526a8ab2ae62e3c31e1c383c082fc1d
Sam Baker <rb.sam@icloud.com>
2021-04-25 12:44:58 +0100

    added 404 asset

--- app.py
+++ app.py
@@ -70,11 +70,6 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 
-@app.errorhandler(403)
-def page_not_found(e):
-    return render_template('403.html'), 403
-
-
 @app.route("/", methods=["GET", "POST"])
 def index():
     # GETS MOST RECENT SUBMISSION
